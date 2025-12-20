81ecf9ae5536768daa835e19fe99618b637acd21
pradeepgaud <pradeepgaur1825@gmail.com>
2025-12-20 10:02:20 +0530

    Update AuthRoutes.js

--- Backend/Routes/AuthRoutes.js
+++ Backend/Routes/AuthRoutes.js
@@ -13,12 +13,19 @@ import userAuth from "../Middleware/UserAuth.js";
 
 const authRouter = express.Router();
 
+// ---------------- AUTH ----------------
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
-authRouter.post("/send-verify-otp", userAuth, sendVerifyOpt);
-authRouter.post("/verify-account", userAuth, verifyEmail);
+
+// ❌ userAuth removed (VERY IMPORTANT)
+authRouter.post("/send-verify-otp", sendVerifyOpt);
+authRouter.post("/verify-account", verifyEmail);
+
+// ---------------- CHECK LOGIN ----------------
 authRouter.get("/is-auth", userAuth, isAuthenticated);
+
+// ---------------- RESET PASSWORD ----------------
 authRouter.post("/send-reset-otp", sendResetOtp);
 authRouter.post("/reset-password", resetPassword);
 
